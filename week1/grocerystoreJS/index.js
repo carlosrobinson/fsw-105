585bafd5727c9c379d4ff802250db8926bad27e6
carlosrobinson <carlos.robinson@bryanuniversity.edu>
2020-10-21 16:40:01 -0400

    first comit

--- week1/grocerystoreJS/index.js
+++ week1/grocerystoreJS/index.js
@@ -4,7 +4,7 @@ var shopper = {
     isAlive : true,
     groceryCart: ["Meat", "Dairy", "Fruit"],
     showCart: function() {
-        return this.items + " " + this.price + " " + this.groceryCart[2];
+        return this.items + " " + this.price + " " + this.isAlive + " " + this.groceryCart[2];
 }
 };
-console.log(shopper.items + " " + shopper.price + " " + shopper.groceryCart[2])
+console.log(shopper.items + " " + shopper.price + " " + shopper.isAlive + " "  + shopper.groceryCart[2])
