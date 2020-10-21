59ac4514b89f4f7920e1c0931c6d4cf5e566e7ea
carlosrobinson <carlos.robinson@bryanuniversity.edu>
2020-10-21 18:45:58 -0400

    first commit

--- week1/grocerystoreJS/index.js
+++ week1/grocerystoreJS/index.js
@@ -1,10 +1,10 @@
-var shopper = {
+ var shopper = {
     items: "Apple",
     price: .99,
     isAlive : true,
     groceryCart: ["Meat", "Dairy", "Fruit"],
     showCart: function() {
-        return this.items + " " + this.price + " " + this.isAlive + " " + this.groceryCart[2];
+        return this.items + " ," + this.price + " ," + this.isAlive + " ," + this.groceryCart[2];
 }
 };
-console.log(shopper.items + " " + shopper.price + " " + shopper.isAlive + " "  + shopper.groceryCart[2])
+console.log(shopper.items + " ," + shopper.price + " ," + shopper.isAlive + " ,"  + shopper.groceryCart[2])
