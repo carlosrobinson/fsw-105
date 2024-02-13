119f1426b470113cf793efffc2c9fed555bbd54a
“carlosrobinson” <robinson.carlos38@gmial.com>
2024-02-13 18:34:32 -0500

    made changes to fsw-105/week1/grocerystoreJS

--- week1/grocerystoreJS/index.js
+++ week1/grocerystoreJS/index.js
@@ -1,3 +1,5 @@
+
+// Object 1 - with primative and complex types
 var shopper = {
     items: "Apple",
     price: .99,
@@ -8,3 +10,44 @@ var shopper = {
 }
 };
 console.log(shopper.items + " ," + shopper.price + " ," + shopper.isAlive + " ,"  + shopper.groceryCart[2])
+
+
+// Object 2 - with primative and complex types
+var shopper = {
+    name: "Steve Johonson",
+    info: {
+        id: 1,
+        email: "steve.johnson@email.com",
+        password: "passwor123"
+    },
+    cart: [
+    {
+        item: "milk",
+        price: 2.33
+    },
+    {
+        item: "pork chops",
+        price: 5.97
+    },
+    {
+        item: "macaroni",
+        price: 2.29
+    },
+    {
+        item: "Dawn",
+        price: 3.45
+    }
+    ],
+    isCartEmpty: false,
+    checkOut: function(){
+        var total = 0;
+        for(var i = 0; i < shopper.cart.length; i++){
+            total += shopper.cart[i].price;
+        }
+        console.log(`Your total is $${total.toFixed(2)}.\nDo you wish to purchase now.`);
+    }
+}
+shopper.checkOut();
+
+
+
